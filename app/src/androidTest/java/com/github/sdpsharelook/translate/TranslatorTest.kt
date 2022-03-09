71242545170c847eff80ce65cc3aca4661700ff0
villemontc <cedric.villemont@gmail.com>
2022-03-10 00:27:13 +0100

    Translator implant using mlkit
    Firstly constructor is defined using src and dst language, we might use detection
    language later to avoid this.

---
+++ app/src/androidTest/java/com/github/sdpsharelook/translate/TranslatorTest.kt
@@ -0,0 +1,33 @@
+package com.github.sdpsharelook.translate
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.mlkit.nl.translate.TranslateLanguage
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class TranslatorTest {
+    @Test
+    fun translatorTest() {
+        val t = Translator(TranslateLanguage.FRENCH, TranslateLanguage.ENGLISH);
+
+        t.translate("Bonjour.", object : TranslateListener {
+            override fun onError(e: Exception) {
+                throw e
+            }
+
+            override fun onTranslated(translatedText: String) {
+                assertEquals("Hello.", translatedText);
+            }
+        })
+
+        Thread.sleep(2000);
+    }
+}

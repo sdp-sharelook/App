caee9c17303ddfeb8c56dd3a8477244071c4faf2
AdrianUlrich <adrian.schemel888@gmail.com>
2022-03-11 08:49:48 +0100

    implement word object comparison

--- app/src/main/java/com/github/sdpsharelook/Word.kt
+++ app/src/main/java/com/github/sdpsharelook/Word.kt
@@ -4,4 +4,21 @@ class Word(
     val name: String,
     val definition: String,
     val isFavourite: Boolean = false
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Word
+
+        return name == other.name
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Word(${if (isFavourite) "fav/" else ""}$name=${definition.take(70)}${if (definition.length > 70) "..." else ""})"
+    }
+}

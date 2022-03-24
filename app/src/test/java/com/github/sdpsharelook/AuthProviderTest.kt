bee685a47e2f0820baa8e1d1bd2a6356201e7e5c
Albert Koppelmaa <albert.koppelmaa@hotmail.com>
2022-03-24 15:57:59 +0100

    fix more merge bugs including android manifest and gradle versions

--- app/src/test/java/com/github/sdpsharelook/AuthProviderTest.kt
+++ app/src/test/java/com/github/sdpsharelook/AuthProviderTest.kt
@@ -7,11 +7,11 @@ import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_PASS
 import com.github.sdpsharelook.authorization.User
 import com.github.sdpsharelook.authorization.auth
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class AuthProviderTest {
     @Before
     fun resetAuthProvider(){
@@ -20,16 +20,16 @@ class AuthProviderTest {
     @Test
     fun testLogin(){
 
-        runBlocking{
-            auth.signInWithEmailAndPassword(TEST_USER_EMAIL, TEST_USER_PASS)
+        runTest{
+            val user = auth.signInWithEmailAndPassword(TEST_USER_EMAIL, TEST_USER_PASS)
+            assert(auth.currentUser!!.email==TEST_USER_EMAIL)
+            auth.signOut();
+            assert(auth.currentUser==null)
         }
-        assert(auth.currentUser!!.email==TEST_USER_EMAIL)
-        auth.signOut();
-        assert(auth.currentUser==null)
     }
     @Test
     fun testInvalidLogin(){
-        runBlocking {
+        runTest {
             val result = auth.signInWithEmailAndPassword(TEST_USER_EMAIL, "")
             assert(result.isFailure)
         }

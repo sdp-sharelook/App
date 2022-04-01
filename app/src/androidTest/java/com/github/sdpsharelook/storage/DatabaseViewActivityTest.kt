0f61da5b3f9c272a794eede8c871e67ae1dca197
AdrianUlrich <adrian.schemel888@gmail.com>
2022-04-01 03:50:32 +0200

    check database activity button

--- app/src/androidTest/java/com/github/sdpsharelook/storage/DatabaseViewActivityTest.kt
+++ app/src/androidTest/java/com/github/sdpsharelook/storage/DatabaseViewActivityTest.kt
@@ -1,11 +1,14 @@
 package com.github.sdpsharelook.storage
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.sdpsharelook.MainActivity
-import org.junit.Assert.*
+import com.github.sdpsharelook.R
 import org.junit.Rule
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,5 +20,8 @@ class DatabaseViewActivityTest {
 
     @Test
     fun testPrintsDatabaseContents() {
+        onView(withId(R.id.fab)).perform(click())
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(isDisplayed()))
     }
 }

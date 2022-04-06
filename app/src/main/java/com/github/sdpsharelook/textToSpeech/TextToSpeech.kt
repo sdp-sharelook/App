06fa422eba97aae3002800834a23ecb2b009857c
Romain Birling <romain@adaptyvbio.com>
2022-04-06 10:56:30 +0200

    refactoring and features improvement in tts

--- app/src/main/java/com/github/sdpsharelook/textToSpeech/TextToSpeech.kt
+++ app/src/main/java/com/github/sdpsharelook/textToSpeech/TextToSpeech.kt
@@ -2,87 +2,55 @@ package com.github.sdpsharelook.textToSpeech
 
 import android.content.Context
 import android.widget.*
-import java.util.*
+import com.github.sdpsharelook.language.Language
 import android.speech.tts.TextToSpeech as GoogleTextToSpeech
 
-class TextToSpeech(val ctx: Context) {
+class TextToSpeech(private val ctx: Context) {
     private var tts: GoogleTextToSpeech? = null
 
     init {
         tts = GoogleTextToSpeech(ctx) {
             when (it) {
                 GoogleTextToSpeech.SUCCESS -> {
-                    tts?.setPitch(0.2f)
-                    tts?.setSpeechRate(1.4f)
-                    tts?.setLanguage(Locale.UK)
+                    tts?.setPitch(pitch)
+                    tts?.setSpeechRate(speechRate)
+                    tts?.language = language.locale
                 }
                 else -> {
-                    Toast.makeText(ctx, "An error happened while creating the TextToSpeech object", Toast.LENGTH_SHORT)
+                    Toast.makeText(
+                        ctx,
+                        "An error happened while creating the TextToSpeech object",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
     }
 
-    /*private fun putLanguagesInSpinner(spinner: Spinner?): Map<String, String> {
-        val languages = tts?.availableLanguages ?: setOf()
-        val nameToTag = languages.map { it.displayLanguage to it.toLanguageTag() }.toMap()
-        val adapter: ArrayAdapter<String> =
-            ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item,
-                nameToTag.keys.toList().filter { it.trim() != "" }
-                    ?: listOf("No language available"))
-        spinner?.setAdapter(adapter)
-        return nameToTag
-    }*/
-
-    /*private fun bindSpinnerLanguages(nameToTag: Map<String, String>?, spinner: Spinner?) {
-        spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                val name = parent.getItemAtPosition(position).toString()
-                nameToTag?.get(name)?.let {
-                    val selectedLanguage = Locale.forLanguageTag(it)
-                    tts?.setLanguage(selectedLanguage)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-            }
+    private var _language = Language.default
+    var language
+        get() = _language
+        set(value) {
+            _language = value
+            tts?.language = value.locale
         }
-    }*/
-
-    /*    fun setupSpinnerLanguages(spinner: Spinner?) {
-            bindSpinnerLanguages(putLanguagesInSpinner(spinner), spinner)
-        }*/
-
-    /*fun bindSeekBars(seekBarPitch: SeekBar?, seekBarSpeechRate: SeekBar?) {
-        // spinner?.adapter = adapter
-        val seekBarsListener = object : SeekBar.OnSeekBarChangeListener {
-
-            override fun onProgressChanged(seekBar: SeekBar?, i: Int, b: Boolean) {
-                if (seekBar === seekBarPitch)
-                    tts?.setPitch(i.toFloat() / 5)
-                else if (seekBar == seekBarSpeechRate)
-                    tts?.setSpeechRate(i.toFloat() / 5)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+    private var _speechRate = .5f
+    var speechRate
+        get() = _speechRate
+        set(value) {
+            _speechRate = value
+            tts?.setSpeechRate(value)
         }
-
-        seekBarPitch?.setOnSeekBarChangeListener(seekBarsListener)
-        seekBarSpeechRate?.setOnSeekBarChangeListener(seekBarsListener)
-    }*/
-
-    fun setLanguage(loc: Locale) = tts?.setLanguage(loc)
-
+    private var _pitch = .5f
+    var pitch
+        get() = _pitch
+        set(value) {
+            _pitch = value
+            tts?.setPitch(value)
+        }
 
     fun speak(message: String) =
         tts?.speak(message, GoogleTextToSpeech.QUEUE_FLUSH, null, null)
-            ?: Toast.makeText ( ctx,"The TextToSpeech object is null", Toast.LENGTH_SHORT).show()
+            ?: Toast.makeText(ctx, "The TextToSpeech object is null", Toast.LENGTH_SHORT).show()
 
 }

59a8edccd8ef6927b69ad8440179030da42d0b1f
Albert Koppelmaa <albert.koppelmaa@hotmail.com>
2022-04-07 20:29:26 +0200

    fixed cache file path

--- app/src/androidTest/java/com/github/sdpsharelook/SRAlgo/SRAlgoTest.kt
+++ app/src/androidTest/java/com/github/sdpsharelook/SRAlgo/SRAlgoTest.kt
@@ -1,10 +1,14 @@
 package com.github.sdpsharelook.SRAlgo
 
+import android.content.Context
+import android.provider.Telephony.Mms.Part.FILENAME
+import androidx.test.core.app.ApplicationProvider
 import com.github.sdpsharelook.revision.SRAlgo
 import com.github.sdpsharelook.revision.revisionWord
 import org.junit.Before
 import org.junit.Test
 import java.io.File
+import java.io.FileOutputStream
 
 class SRAlgoTest {
 
@@ -18,23 +22,25 @@ class SRAlgoTest {
     @Before
     fun clearTest() {
         File(TEST_SR_FILE).delete()
+        println(ApplicationProvider.getApplicationContext<Context?>().applicationContext.filesDir)
         TEST_WORDS.forEach {
-            it.saveToStorage(TEST_SR_FILE)
+            it.saveToStorage(ApplicationProvider.getApplicationContext(),TEST_SR_FILE)
         }
     }
 
     @Test
     fun getWords() {
-        val revWords = SRAlgo.loadRevWordsFromLocal(TEST_SR_FILE)
+        val revWords = SRAlgo.loadRevWordsFromLocal(ApplicationProvider.getApplicationContext(),TEST_SR_FILE)
         assert(revWords == TEST_WORDS)
     }
+
     @Test
     fun checkNextReviewTime() {
         val revW = TEST_WORDS[0]
-        for(i in 1..5){
-            revW.n=i
+        for (i in 1..5) {
+            revW.n = i
             SRAlgo.calcNextReviewTime(revW, 5)
         }
-        assert(revW.EF <= SRAlgo.MAX_EF && revW.EF>= SRAlgo.MIN_EF)
+        assert(revW.EF <= SRAlgo.MAX_EF && revW.EF >= SRAlgo.MIN_EF)
     }
 }

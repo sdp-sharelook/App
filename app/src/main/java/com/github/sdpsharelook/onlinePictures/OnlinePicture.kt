f98840f6fa8d3501e53cf96726ac8f30131fd874
Romain Birling <rombirli@gmail.com>
2022-04-15 14:55:26 +0200

    Implemented a first version of a online-picture-picker. - Tests are coming soon

---
+++ app/src/main/java/com/github/sdpsharelook/onlinePictures/OnlinePicture.kt
@@ -0,0 +1,43 @@
+package com.github.sdpsharelook.onlinePictures
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import com.github.sdpsharelook.R
+import java.io.InputStream
+import java.net.URL
+
+/**
+ *
+ */
+data class OnlinePicture(
+    /**
+     * the link to the thumbnail picture
+     */
+    val thumbnailLink: String,
+    /**
+     * the source (domain) of the website hosting the picture
+     */
+    val mediumLink: String,
+    /**
+     * the link of the website hosting the picture
+     */
+    val title: String,
+) {
+    /**
+     * fetch the thumbnail bitmap from the link in 'thumbnail'
+     */
+    val thumbnail: Bitmap by lazy {
+        try {
+            BitmapFactory.decodeStream(URL(thumbnailLink).content as InputStream)
+        } catch (e: Throwable) {
+            BitmapFactory.decodeResource(null, R.drawable.ic_no_image);
+        }
+    }
+
+    /**
+     * fetch the original bitmap from the link in 'original'
+     */
+    val medium: Bitmap by lazy {
+        BitmapFactory.decodeStream(URL(mediumLink).content as InputStream)
+    }
+}

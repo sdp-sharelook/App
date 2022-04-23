d82192d77b0304b542e7e9b5019cb59e11b8ac64
AdrianUlrich <adrian.schemel@epfl.ch>
2022-04-23 20:26:25 +0200

    add hilt

---
+++ app/src/main/java/com/github/sdpsharelook/App.kt
@@ -0,0 +1,14 @@
+package com.github.sdpsharelook
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+import dagger.hilt.android.HiltAndroidApp
+
+@Suppress("unused")
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}

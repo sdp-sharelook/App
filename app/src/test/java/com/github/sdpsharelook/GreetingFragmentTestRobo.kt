007f55c3627119bc50a71e89ee161abc5e292484
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-02 22:32:37 +0200

    make example robolectric fragment test
    it runs fast and good

---
+++ app/src/test/java/com/github/sdpsharelook/GreetingFragmentTestRobo.kt
@@ -0,0 +1,23 @@
+package com.github.sdpsharelook
+
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class GreetingFragmentTestRobo {
+    @Test
+    fun testHelloWorld() {
+        launchFragmentInContainer<GreetingFragment>(
+            bundleOf("name" to "World"),
+            R.style.Theme_Sherlook
+        )
+        onView(withId(R.id.greetingMessage)).check(matches(withText("Hello World!")))
+    }
+}

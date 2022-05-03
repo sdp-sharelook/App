bf8c47c8c10b5a1245a47948790dbe65e48a553a
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-03 11:01:21 +0200

    launch in Hilt container

---
+++ app/src/debug/java/com/github/sdpsharelook/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.github.sdpsharelook
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()

sdp-sharelook/App

commit 344e1273e5c2acde677d3ac9424d5b0c671c0b78

inject and test signup fragment

app/src/main/java/com/github/sdpsharelook/authorization/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.github.sdpsharelook.authorization
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,11 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.github.sdpsharelook.databinding.FragmentSignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class SignUpFragment : Fragment() {
 
     /**
@@ -86,9 +89,9 @@ class SignUpFragment : Fragment() {
     }
 
     private fun prelimPasswordListener() {
-        binding.prelimpassword.setOnFocusChangeListener { _, focus ->
+        binding.password.setOnFocusChangeListener { _, focus ->
             if (!focus) {
-                binding.prelimpassword.error = isPrelimPassword()
+                binding.password.error = isPrelimPassword()
                 binding.prelimPasswordBox.helperText = isPrelimPassword()
             }
         }
@@ -100,17 +103,17 @@ class SignUpFragment : Fragment() {
         val oneDigit = "(.*?[0-9].*)".toRegex()
         val specialChar = "(.*?[#?!()@\$ %^&*-].*)".toRegex()
         val minLength8 = ".{8,}".toRegex()
-        return if (binding.prelimpassword.text.toString() == "")
+        return if (binding.password.text.toString() == "")
             "Required"
-        else if (!binding.prelimpassword.text.toString().matches(upperCase))
+        else if (!binding.password.text.toString().matches(upperCase))
             "Must contain an uppercase letter"
-        else if (!binding.prelimpassword.text.toString().matches(lowerCase))
+        else if (!binding.password.text.toString().matches(lowerCase))
             "Must contain a lowercase letter"
-        else if (!binding.prelimpassword.text.toString().matches(oneDigit))
+        else if (!binding.password.text.toString().matches(oneDigit))
             "Must contain at least one digit"
-        else if (!binding.prelimpassword.text.toString().matches(specialChar))
+        else if (!binding.password.text.toString().matches(specialChar))
             "Must contain at least one special character"
-        else if (!binding.prelimpassword.text.toString().matches(minLength8))
+        else if (!binding.password.text.toString().matches(minLength8))
             "Must contain at least 8 characters"
         else
             null
@@ -125,15 +128,9 @@ class SignUpFragment : Fragment() {
         }
     }
 
-    private fun isPasswordValid(): String? {
-        when {
-            binding.password.text.toString() != binding.prelimpassword.text.toString() -> {
-                return "Passwords do not match"
-            }
-            else -> {
-                return null
-            }
-        }
+    private fun isPasswordValid(): String? = when {
+        binding.password.text.toString() != binding.confirmPassword.text.toString() -> "Passwords do not match"
+        else -> null
     }
 
     private fun checkBeforeSignUp() {
@@ -143,6 +140,7 @@ class SignUpFragment : Fragment() {
         val emailValid = binding.email.error == null
         val prelimPassValid = binding.prelimPasswordBox.helperText == null
         val passValid = binding.password.error == null && binding.passwordBox.helperText == null
+        Log.e("REGISTER","checks: $firstNameValid $lastNameValid $emailValid $prelimPassValid $passValid")
         if (firstNameValid && lastNameValid && emailValid && prelimPassValid && passValid) {
             signUp()
         } else {
@@ -154,7 +152,7 @@ class SignUpFragment : Fragment() {
         binding.firstName.error = isFirstNameValid()
         binding.lastName.error = isLastNameValid()
         binding.email.error = isEmailValid()
-        binding.prelimpassword.error = isPrelimPassword()
+        binding.password.error = isPrelimPassword()
         binding.prelimPasswordBox.helperText = isPrelimPassword()
         binding.passwordBox.error = isPasswordValid()
         binding.passwordBox.helperText = isPasswordValid()

app/src/test/java/com/github/sdpsharelook/authorization/LoginTestRobo.kt
@@ -6,8 +6,8 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.sdpsharelook.R
-import com.github.sdpsharelook.authorization.UserConstants.TEST_USER_EMAIL
-import com.github.sdpsharelook.authorization.UserConstants.TEST_USER_PASS
+import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_EMAIL
+import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_PASS
 import com.github.sdpsharelook.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -17,11 +17,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import javax.inject.Inject
-
-object UserConstants {
-    const val TEST_USER_EMAIL = "testuser@gmail.com"
-    const val TEST_USER_PASS = "123456"
-}
 
 @ExperimentalCoroutinesApi
 @HiltAndroidTest
@@ -44,7 +39,7 @@ class LoginTestRobo {
     fun `test login with test user`() {
         launchFragmentInHiltContainer<LoginFragment>(Bundle(), R.style.Theme_Sherlook)
         onView(withId(R.id.email)).perform(typeText(TEST_USER_EMAIL))
-        onView(withId(R.id.password)).perform(typeText(TEST_USER_PASS))
+        onView(withId(R.id.confirmPassword)).perform(typeText(TEST_USER_PASS))
         onView(withId(R.id.loginButton)).perform(click())
         assert(auth.currentUser!!.email == TEST_USER_EMAIL)
     }
@@ -54,7 +49,7 @@ class LoginTestRobo {
         launchFragmentInHiltContainer<LoginFragment>(Bundle(), R.style.Theme_Sherlook)
         onView(withId(R.id.loginButton)).perform(swipeLeft())
         onView(withId(R.id.email)).perform(typeText(""))
-        onView(withId(R.id.password)).perform(typeText(TEST_USER_PASS))
+        onView(withId(R.id.confirmPassword)).perform(typeText(TEST_USER_PASS))
         onView(withId(R.id.loginButton)).perform(click()).also {
             assert(auth.currentUser == null)
         }

app/src/androidTest/java/com/github/sdpsharelook/authorization/LoginTest.kt
@@ -1,19 +1,6 @@
 package com.github.sdpsharelook.authorization
 
-import android.os.Bundle
-import androidx.fragment.app.testing.FragmentScenario
-import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.sdpsharelook.R
-import com.github.sdpsharelook.authorization.UserConstants.TEST_USER_EMAIL
-import com.github.sdpsharelook.authorization.UserConstants.TEST_USER_PASS
-import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.inject.Provider
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 

app/src/main/res/layout/fragment_sign_up.xml
@@ -21,7 +21,7 @@
             android:id="@+id/firstName"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="First Name"
+            android:hint="@string/first_name"
             android:inputType="textPersonName" />
     </com.google.android.material.textfield.TextInputLayout>
 
@@ -43,7 +43,7 @@
             android:id="@+id/lastName"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="Last Name"
+            android:hint="@string/last_name"
             android:inputType="textPersonName" />
     </com.google.android.material.textfield.TextInputLayout>
 
@@ -65,7 +65,7 @@
             android:id="@+id/email"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="Email"
+            android:hint="@string/email"
             android:inputType="textEmailAddress" />
     </com.google.android.material.textfield.TextInputLayout>
 
@@ -89,7 +89,7 @@
             android:id="@+id/phoneNumber"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="Phone Number"
+            android:hint="@string/phone_number"
             android:inputType="phone"
             android:maxLength="10" />
     </com.google.android.material.textfield.TextInputLayout>
@@ -112,10 +112,10 @@
         app:layout_constraintTop_toBottomOf="@+id/phoneNumberBox">
 
         <com.google.android.material.textfield.TextInputEditText
-            android:id="@+id/prelimpassword"
+            android:id="@+id/password"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="Password"
+            android:hint="@string/password"
             android:inputType="textPassword"
             android:windowSoftInputMode="adjustPan" />
     </com.google.android.material.textfield.TextInputLayout>
@@ -138,10 +138,10 @@
         app:layout_constraintTop_toBottomOf="@+id/prelimPasswordBox">
 
         <com.google.android.material.textfield.TextInputEditText
-            android:id="@+id/password"
+            android:id="@+id/confirmPassword"
             android:layout_width="match_parent"
             android:layout_height="wrap_content"
-            android:hint="Repeat Password"
+            android:hint="@string/repeat_password"
             android:inputType="textPassword"
             android:windowSoftInputMode="adjustPan" />
     </com.google.android.material.textfield.TextInputLayout>
@@ -155,7 +155,7 @@
         android:layout_marginTop="30dp"
         android:layout_marginEnd="60dp"
         android:checkable="false"
-        android:text="Sign Up !"
+        android:text="@string/sign_up"
         app:iconPadding="0dp"
         app:layout_constraintEnd_toEndOf="parent"
         app:layout_constraintHorizontal_bias="0.1"

app/src/main/res/values/strings.xml
@@ -129,4 +129,11 @@
     <string name="section_name_hint">Section name</string>
     <string name="word_image">Image</string>
     <string name="save_section_name">Save</string>
+    <string name="first_name">First Name</string>
+    <string name="last_name">Last Name</string>
+    <string name="email">Email</string>
+    <string name="phone_number">Phone Number</string>
+    <string name="password">Password</string>
+    <string name="repeat_password">Repeat Password</string>
+    <string name="sign_up">Sign Up !</string>
 </resources>

app/src/test/java/com/github/sdpsharelook/authorization/SignUpTestRobo.kt
@@ -1,0 +1,102 @@
+package com.github.sdpsharelook.authorization
+
+
+import android.os.Bundle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.sdpsharelook.R
+import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_EMAIL
+import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_PASS
+import com.github.sdpsharelook.authorization.TestUserConstants.TEST_USER_PASS2
+import com.github.sdpsharelook.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.Matchers.allOf
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+const val NEW_USER_EMAIL = "testuser123@gmail.com"
+const val NEW_USER_PASS = "123456"
+
+@ExperimentalCoroutinesApi
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class SignUpTestRobo {
+
+    @Inject
+    lateinit var auth: AuthProvider
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+        launchFragmentInHiltContainer<SignUpFragment>(Bundle(), R.style.Theme_Sherlook)
+    }
+
+    @After
+    fun cleanUp() {
+        auth.signOut()
+    }
+
+    @Test
+    fun `test sign up is displayed`() = runTest {
+        launchFragmentInHiltContainer<SignUpFragment>(Bundle(), R.style.Theme_Sherlook)
+        onView(withId(R.id.layout_signup)).check(matches(isDisplayed()))
+        onView(withId(R.id.email)).check(matches(isDisplayed()))
+        onView(withId(R.id.emailBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.firstName)).check(matches(isDisplayed()))
+        onView(withId(R.id.firstNameBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.lastName)).check(matches(isDisplayed()))
+        onView(withId(R.id.lastNameBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.confirmPassword)).check(matches(isDisplayed()))
+        onView(withId(R.id.passwordBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.password)).check(matches(isDisplayed()))
+        onView(withId(R.id.prelimPasswordBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.phoneNumber)).check(matches(isDisplayed()))
+        onView(withId(R.id.phoneNumberBox)).check(matches(isDisplayed()))
+        onView(withId(R.id.loginButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `test sign up`() = runTest {
+        launchFragmentInHiltContainer<SignUpFragment>(Bundle(), R.style.Theme_Sherlook) {
+            auth
+        }
+        onView(withId(R.id.firstName)).perform(replaceText("Jean"), closeSoftKeyboard())
+        onView(withId(R.id.lastName)).perform(replaceText("Paul"), closeSoftKeyboard())
+//        onView(withId(R.id.phoneNumber)).perform(replaceText("000 000 00 00"), closeSoftKeyboard())
+        onView(withId(R.id.email)).perform(replaceText(TEST_USER_EMAIL), closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(replaceText(TEST_USER_PASS), closeSoftKeyboard())
+        onView(withId(R.id.confirmPassword)).perform(replaceText(TEST_USER_PASS), closeSoftKeyboard())
+        onView(allOf(withId(R.id.loginButton), withText("Sign Up !"))).perform(click())
+        onView(withId(R.id.email)).perform(replaceText(NEW_USER_EMAIL), closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(replaceText(TEST_USER_PASS2), closeSoftKeyboard())
+        onView(withId(R.id.confirmPassword)).perform(replaceText(TEST_USER_PASS2), closeSoftKeyboard())
+        onView(allOf(withId(R.id.loginButton), withText("Sign Up !"))).perform(click())
+        assertNotNull(auth.currentUser)
+        assertEquals(NEW_USER_EMAIL, auth.currentUser!!.email)
+    }
+
+    @Test
+    fun `test sign up no email`() = runTest {
+        launchFragmentInHiltContainer<SignUpFragment>(Bundle(), R.style.Theme_Sherlook)
+        onView(withId(R.id.email)).perform(replaceText(" "), closeSoftKeyboard())
+        onView(withId(R.id.confirmPassword)).perform(replaceText(TEST_USER_PASS), closeSoftKeyboard())
+        onView(allOf(withId(R.id.loginButton), withText("Sign Up !"))).perform(click())
+        onView(allOf(withId(R.id.loginButton), withText("Sign Up !"))).perform(click())
+        assertNull(auth.currentUser)
+    }
+
+}

commit cebd759935e7cba345fd79276925f77675998c21

fix login password view id
(caused by commit 344e1273e5c2acde677d3ac9424d5b0c671c0b78)

app/src/main/java/com/github/sdpsharelook/authorization/SignUpFragment.kt
@@ -1,7 +1,6 @@
 package com.github.sdpsharelook.authorization
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -140,7 +139,6 @@ class SignUpFragment : Fragment() {
         val emailValid = binding.email.error == null
         val prelimPassValid = binding.prelimPasswordBox.helperText == null
         val passValid = binding.password.error == null && binding.passwordBox.helperText == null
-        Log.e("REGISTER","checks: $firstNameValid $lastNameValid $emailValid $prelimPassValid $passValid")
         if (firstNameValid && lastNameValid && emailValid && prelimPassValid && passValid) {
             signUp()
         } else {

app/src/test/java/com/github/sdpsharelook/TextDetectionFragmentTest.kt
@@ -1,31 +1,19 @@
 package com.github.sdpsharelook
 
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
-import com.github.sdpsharelook.section.SectionFragment
 import com.github.sdpsharelook.textDetection.TextDetectionFragment
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.robolectric.Robolectric
 import org.robolectric.shadows.ShadowLooper
 
 @ExperimentalCoroutinesApi

app/src/test/java/com/github/sdpsharelook/authorization/LoginTestRobo.kt
@@ -12,6 +12,8 @@ import com.github.sdpsharelook.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -32,24 +34,27 @@ class LoginTestRobo {
     @Before
     fun init() {
         hiltRule.inject()
+        launchFragmentInHiltContainer<LoginFragment>(Bundle(), R.style.Theme_Sherlook)
+    }
+
+    @After
+    fun cleanUp() {
         auth.signOut()
     }
 
     @Test
-    fun `test login with test user`() {
-        launchFragmentInHiltContainer<LoginFragment>(Bundle(), R.style.Theme_Sherlook)
+    fun `test login with test user`() = runTest {
         onView(withId(R.id.email)).perform(typeText(TEST_USER_EMAIL))
-        onView(withId(R.id.confirmPassword)).perform(typeText(TEST_USER_PASS))
+        onView(withId(R.id.password)).perform(typeText(TEST_USER_PASS))
         onView(withId(R.id.loginButton)).perform(click())
         assert(auth.currentUser!!.email == TEST_USER_EMAIL)
     }
 
     @Test
-    fun `test login with no email`() {
-        launchFragmentInHiltContainer<LoginFragment>(Bundle(), R.style.Theme_Sherlook)
+    fun `test login with no email`() = runTest {
         onView(withId(R.id.loginButton)).perform(swipeLeft())
         onView(withId(R.id.email)).perform(typeText(""))
-        onView(withId(R.id.confirmPassword)).perform(typeText(TEST_USER_PASS))
+        onView(withId(R.id.password)).perform(typeText(TEST_USER_PASS))
         onView(withId(R.id.loginButton)).perform(click()).also {
             assert(auth.currentUser == null)
         }
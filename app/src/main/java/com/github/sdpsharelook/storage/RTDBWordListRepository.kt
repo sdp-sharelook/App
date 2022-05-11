a4ba596ea2d023dfc7cee62e62383cea2cb28b61
Albert Koppelmaa <albert.koppelmaa@hotmail.com>
2022-05-11 21:23:17 +0200

    start broken test of wordlist repo

--- app/src/main/java/com/github/sdpsharelook/storage/RTDBWordListRepository.kt
+++ app/src/main/java/com/github/sdpsharelook/storage/RTDBWordListRepository.kt
@@ -3,7 +3,6 @@ package com.github.sdpsharelook.storage
 import com.github.sdpsharelook.Word
 import com.github.sdpsharelook.authorization.AuthProvider
 import com.google.firebase.database.*
-import com.google.firebase.database.ktx.getValue
 import com.google.gson.Gson
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
@@ -37,13 +36,19 @@ class RTDBWordListRepository @Inject constructor(
                 }
 
                 override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                    val list = listOfNotNull(snapshot.getValue<Word>())
-                    trySendBlocking(Result.success(list))
+                    val word = Gson().fromJson(snapshot.value.toString(), Word::class.java)
+                    val oldWord = wordList.find { w ->w.uid==word.uid}
+                    wordList[wordList.indexOf(oldWord)] = word.copy()
+                    trySendBlocking(Result.success(wordList))
                 }
 
-                override fun onChildRemoved(snapshot: DataSnapshot) {
-                    val list: List<Word> = listOfNotNull(snapshot.getValue<Word>())
-                    trySendBlocking(Result.success(list))
+                override fun onChildRemoved(snapshot: DataSnapshot){
+                    val word = Gson().fromJson(snapshot.value.toString(), Word::class.java)
+                    val changedWord = wordList.find {w->
+                        w.uid==word.uid
+                    }
+                    wordList[wordList.indexOf(changedWord)] = word
+                    trySendBlocking(Result.success(wordList))
                 }
 
                 override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
@@ -55,9 +60,9 @@ class RTDBWordListRepository @Inject constructor(
                 }
 
             }
-            databaseReference(name).addChildEventListener(fireListener)
+            getUserReference().addChildEventListener(fireListener)
             awaitClose {
-                databaseReference(name).removeEventListener(fireListener)
+                getUserReference().removeEventListener(fireListener)
             }
         }
 
@@ -65,7 +70,7 @@ class RTDBWordListRepository @Inject constructor(
         val user = auth.currentUser
         //TODO: handle when user not logged
         if (user != null) {
-            return firebaseDatabase.getReference("users/" + user.uid + "/words")
+            return firebaseDatabase.getReference("users/" + user.uid+"/words")
         }
         return firebaseDatabase.getReference("users/guest/words")
     }
@@ -78,22 +83,17 @@ class RTDBWordListRepository @Inject constructor(
      */
     override suspend fun insert(name: String, entity: List<Word>) {
         entity.forEach {
-            getUserReference().child(it.uid).setValue(Gson().toJson(it).toString())
-                .addOnSuccessListener {
-                }
+            getUserReference().child(it.uid).setValue(Gson().toJson(it).toString()).addOnSuccessListener {
+                //TODO: SHOULD MAYBE DO SOMETHING ON SUCCESS ?
+            }
         }
     }
 
-    /**
-     * Don't use
-     */
-    override suspend fun read(name: String): List<Word> {
-        throw UnsupportedOperationException("Use flow function for lists")
-    }
+
 
 
     /**
-     * Update data entry at [name].
+     * Update data entry
      *
      * Note: will not create entry, for that use [insert]
      *
@@ -101,9 +101,7 @@ class RTDBWordListRepository @Inject constructor(
      * @param entity Entity
      */
     override suspend fun update(name: String, entity: List<Word>) {
-        val databaseReference =
-            databaseReference(name)
-        databaseReference.setValue(entity).await()
+        getUserReference().setValue(entity).await()
     }
 
     /**
@@ -112,11 +110,21 @@ class RTDBWordListRepository @Inject constructor(
      * @param name identifier of entity
      */
     override suspend fun delete(name: String) {
-        databaseReference(name).removeValue().await()
+        getUserReference().removeValue().await()
     }
 
-    private fun databaseReference(name: String): DatabaseReference {
+    fun databaseReference(name: String): DatabaseReference {
         return if (name == "test") getUserReference() else reference.child(name)
+    }
+
+    /**
+     * Read data at [name] once asynchronously.
+     *
+     * @param name identifier of entity
+     * @return [List] of [Word] or null
+     */
+    override suspend fun read(name: String): List<Word>? {
+        TODO("Not yet implemented")
     }
 
 }

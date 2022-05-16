ebae4958a009c0c6d363793f20185d0664e9397d
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-16 13:34:16 +0200

    test greeting activity

--- app/src/test/java/com/github/sdpsharelook/GreetingFragmentTestRobo.kt
+++ app/src/test/java/com/github/sdpsharelook/GreetingFragmentTestRobo.kt
@@ -6,7 +6,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.sdpsharelook.utils.launchFragmentInHiltContainer
+import com.github.sdpsharelook.utils.FragmentScenarioRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,6 +23,9 @@ class GreetingFragmentTestRobo {
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
 
+    @get:Rule(order = 1)
+    val fragmentScenarioRule = FragmentScenarioRule.launch(GreetingFragment::class,null,bundleOf("name" to "World"))
+
     @Before
     fun init() {
         hiltRule.inject()
@@ -30,10 +33,6 @@ class GreetingFragmentTestRobo {
 
     @Test
     fun testHelloWorld() {
-        launchFragmentInHiltContainer<GreetingFragment>(
-            bundleOf("name" to "World"),
-            R.style.Theme_Sherlook
-        )
         onView(withId(R.id.greetingMessage)).check(matches(withText("Hello World!")))
     }
 }

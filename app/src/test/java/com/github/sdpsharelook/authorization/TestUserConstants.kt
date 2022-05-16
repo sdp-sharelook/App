060e34605570ef4fe214a19c2ae7c42acbea8bfd
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-16 11:08:07 +0200

    fix signup test

--- app/src/test/java/com/github/sdpsharelook/authorization/TestUserConstants.kt
+++ app/src/test/java/com/github/sdpsharelook/authorization/TestUserConstants.kt
@@ -2,7 +2,8 @@ package com.github.sdpsharelook.authorization
 
 object TestUserConstants {
     const val TEST_USER_EMAIL = "testuser@gmail.com"
+    const val NEW_USER_EMAIL = "testuser123@gmail.com"
     const val TEST_USER_UID = "DSKjhf23dskjfhalfaJFSD"
     const val TEST_USER_PASS = "123456"
-    const val TEST_USER_PASS2 = "Abcdef1!"
+    const val NEW_USER_PASS = "Abcdef1!"
 }

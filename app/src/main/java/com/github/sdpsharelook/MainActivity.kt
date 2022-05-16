f54b4380454afcc870d8b4fb467d8acc29470905
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-16 14:26:17 +0200

    fix theme and fragment view

--- app/src/main/java/com/github/sdpsharelook/MainActivity.kt
+++ app/src/main/java/com/github/sdpsharelook/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,7 +29,8 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
         val navView = findViewById<NavigationView>(R.id.navView)
-        val navController = Navigation.findNavController(this, R.id.navHostFragment)
+        val navController = supportFragmentManager.findFragmentById(R.id.navHostFragment)!!.findNavController()
+//        val navController = Navigation.findNavController(this, R.id.navHostFragment)
         NavigationUI.setupWithNavController(navView, navController)
     }
 }

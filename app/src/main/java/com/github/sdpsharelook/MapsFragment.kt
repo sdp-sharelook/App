f52d9a9bd1343bdd346add4d2ad32145c176d7e4
Saged <saged.bounekhel@epfl.ch>
2022-05-20 07:49:48 +0200

    redesign popup

--- app/src/main/java/com/github/sdpsharelook/MapsFragment.kt
+++ app/src/main/java/com/github/sdpsharelook/MapsFragment.kt
@@ -11,6 +11,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
@@ -25,6 +26,12 @@ import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
 import com.github.sdpsharelook.language.Language
 import androidx.fragment.app.FragmentManager
+import com.github.sdpsharelook.authorization.AuthProvider
+import com.github.sdpsharelook.authorization.FireAuth
+import com.github.sdpsharelook.storage.RTDBWordListRepository
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.coroutines.runBlocking
 
 class MapsFragment : Fragment() {
 
@@ -41,21 +48,30 @@ class MapsFragment : Fragment() {
          * user has installed Google Play services and returned to the app.
          */
 
+        val db = RTDBWordListRepository()
         var words = listOf<Word>()
+        val authProvider = FireAuth()
+        val user = authProvider.currentUser
+        if (user != null) {
+            runBlocking {
+                words = db.read(user.displayName)
+            }
+        }
+
 
         // ===================================================================================
         // Just for demo purpose
-        val pic1 = BitmapFactory.decodeResource(resources, R.drawable.download)
+        val pic1 = BitmapFactory.decodeResource(resources, R.drawable.photo1)
         val date1 = Date(1651810934000)
         val loc1 = Location("loc1") //.setLatitude(46.524107017942995), 6.560875856078083
         loc1.latitude = 46.524107017942995
         loc1.longitude = 6.560875856078083
-        val pic2 = BitmapFactory.decodeResource(resources, R.drawable.downloadp)
+        val pic2 = BitmapFactory.decodeResource(resources, R.drawable.photo2)
         val date2 = Date(1604722934000)
         val loc2 = Location("loc2")
         loc2.latitude = 41.90706398797125
         loc2.longitude = 12.48717493069279
-        val pic3 = BitmapFactory.decodeResource(resources, R.drawable.downloadpp)
+        val pic3 = BitmapFactory.decodeResource(resources, R.drawable.photo3)
         val date3 = Date(1455337334000)
         val loc3 = Location("loc3")
         loc3.latitude = 51.22670535104726
@@ -66,8 +82,6 @@ class MapsFragment : Fragment() {
         words = words.plus(por).plus(por2).plus(por3)
         // Just for demo purpose
         // ===========================================================================================================
-        // TODO: Fill words with real list of words in the database
-        // TODO: Check why Picture is a String and not a Bitmap in Word
 
         val sydney = LatLng(46.51887158482739, 6.56380824248961)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Inf"))
@@ -104,11 +118,13 @@ class MapsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
-    //@RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    //       val bottomSheet: View = view.findViewById(R.id.bottom_sheet1)
+//        val bottomSheetBehaviour = BottomSheetBehavior.from(bottomSheet)
+//        bottomSheetBehaviour.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
     private fun setMapLongClick(map: GoogleMap) {
@@ -128,18 +144,30 @@ class MapsFragment : Fragment() {
         }
     }
 
-    //@RequiresApi(Build.VERSION_CODES.O)
     private fun openImage(map: GoogleMap) {
         map.setOnMarkerClickListener { marker ->
             val word = hashMap[marker]
           BitmapFactory.decodeResource(requireContext().resources, R.drawable.default_user_path)
             if (word != null) {
-                ImagePopupFragment.newInstance(word.source.toString(),
+                val dialog = ImagePopupFragment.newInstance(word.source.toString(),
                     word.target.toString(),
                     word.savedDate!!,
                     //ContextCompat.getDrawable(context!!, R.drawable.default_user_path)?.toBitmap()!!
                     decodeImage(word.picture!!)
-                ).show(childFragmentManager, ImagePopupFragment.TAG)
+                )
+                dialog.show(childFragmentManager, ImagePopupFragment.TAG)
+
+//                view?.findViewById<LinearLayout>(R.id.sheet)?.getLayoutParams()?.height = ViewGroup.LayoutParams.MATCH_PARENT
+//                val bottomSheetDialog = BottomSheetDialog(requireActivity())
+//                val bottomSheetView = LayoutInflater.from(requireActivity().applicationContext).inflate(
+//                    R.layout.image_popup_layout, view?.findViewById(R.id.sheet) as LinearLayout?
+//                )
+//                bottomSheetView.findViewById<View>(R.id.button3).setOnClickListener {
+//                    bottomSheetDialog.dismiss()
+//                }
+//
+//                bottomSheetDialog.setContentView(bottomSheetView)
+//                bottomSheetDialog.show()
 
             }
             marker.showInfoWindow()
@@ -148,7 +176,6 @@ class MapsFragment : Fragment() {
         }
     }
 
-    //@RequiresApi(Build.VERSION_CODES.O)
     private fun encodeImage(bm: Bitmap): String? {
         val baos = ByteArrayOutputStream()
         bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
@@ -156,7 +183,6 @@ class MapsFragment : Fragment() {
         return Base64.getEncoder().encodeToString(b) //getEncoder().encodeToString(b)
     }
 
-    //@RequiresApi(Build.VERSION_CODES.O)
     private fun decodeImage(s: String): Bitmap {
         var p = Base64.getDecoder().decode(s)
         return BitmapFactory.decodeByteArray(p, 0, p.size)

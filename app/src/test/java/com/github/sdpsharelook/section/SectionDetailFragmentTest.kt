5c8e70f48fdbd8c899f4a3c8715a28f93a8b4d05
Romain Birling <rombirli@gmail.com>
2022-05-22 13:08:02 +0200

    Fixed problem in SectionDetailFragmentTest.kt

--- app/src/test/java/com/github/sdpsharelook/section/SectionDetailFragmentTest.kt
+++ app/src/test/java/com/github/sdpsharelook/section/SectionDetailFragmentTest.kt
@@ -6,6 +6,8 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.sdpsharelook.R
+import com.github.sdpsharelook.Word
+import com.github.sdpsharelook.language.Language
 import com.github.sdpsharelook.storage.IRepository
 import com.github.sdpsharelook.utils.FragmentScenario
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -38,7 +40,7 @@ class SectionDetailFragmentTest {
             "testHelloWorld"
         )
         sectionList.add(0, section)
-        val sectionWord = SectionWord("Hola", "Bonjour", null)
+        val sectionWord = Word("", "Hola", Language("es"), "Bonjour", Language("fr"))
         val fragmentArgs = bundleOf("sectionID" to 0, "sectionWord" to sectionWord)
         FragmentScenario.launch(SectionDetailFragment::class, null, fragmentArgs)
     }

83e4702c122f8a4bc731ebdda2fcd0c5c908e385
Romain Birling <rombirli@gmail.com>
2022-05-25 22:25:57 +0200

    Finished integrated and tested the language downloader fragment

--- app/src/main/java/com/github/sdpsharelook/downloads/MLKitTranslatorDownloader.kt
+++ app/src/main/java/com/github/sdpsharelook/downloads/MLKitTranslatorDownloader.kt
@@ -2,6 +2,8 @@ package com.github.sdpsharelook.downloads
 
 import com.github.sdpsharelook.language.Language
 import com.github.sdpsharelook.translate.MLKitTranslator
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.mlkit.common.model.DownloadConditions
 import com.google.mlkit.common.model.RemoteModelManager
 import com.google.mlkit.nl.translate.TranslateRemoteModel
@@ -15,7 +17,7 @@ class MLKitTranslatorDownloader(
 
     suspend fun downloadedLanguages(): List<Language>? = suspendCoroutine { continuation ->
         modelManager.getDownloadedModels(TranslateRemoteModel::class.java)
-            .addOnSuccessListener { models -> }
+            .addOnSuccessListener { models -> continuation.resume(models.map { Language(it.language) }) }
             .addOnFailureListener { continuation.resume(null) }
     }
 
@@ -35,7 +37,8 @@ class MLKitTranslatorDownloader(
     suspend fun downloadLanguage(language: Language): Boolean =
         downloadedLanguages()?.let { downloadedLanguages ->
             if (language !in MLKitTranslator.availableLanguages) false
-            else if (language !in downloadedLanguages) {
+            else if (language in downloadedLanguages) true
+            else {
                 val model = TranslateRemoteModel.Builder(language.tag).build()
                 val conditionsBuilder = DownloadConditions.Builder()
                 if (requireWifi) conditionsBuilder.requireWifi()
@@ -48,6 +51,8 @@ class MLKitTranslatorDownloader(
                             continuation.resume(false)
                         }
                 }
-            } else true
+            }
         } ?: false
+
+
 }

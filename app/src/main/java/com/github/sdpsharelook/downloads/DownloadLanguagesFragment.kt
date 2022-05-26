eecbf463981e276e983d0413f754fa183b8b9377
Romain Birling <rombirli@gmail.com>
2022-05-26 21:09:02 +0200

    Revert "written skeleton of new tests,"
    This reverts commit d7f07d95ef837f75a248b37b0052211a4b80ea85.

--- app/src/main/java/com/github/sdpsharelook/downloads/DownloadLanguagesFragment.kt
+++ app/src/main/java/com/github/sdpsharelook/downloads/DownloadLanguagesFragment.kt
@@ -10,19 +10,14 @@ import com.github.sdpsharelook.R
 import com.github.sdpsharelook.databinding.FragmentDownloadLanguagesBinding
 import com.github.sdpsharelook.databinding.LayoutLanguageDownloaderBinding
 import com.github.sdpsharelook.language.Language
-import com.github.sdpsharelook.section.SectionFragmentLift
 import com.github.sdpsharelook.translate.MLKitTranslator
-import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
-@AndroidEntryPoint
-class DownloadLanguagesFragment : DownloadLanguagesFragmentLift()
-
-open class DownloadLanguagesFragmentLift : Fragment() {
+class DownloadLanguagesFragment : Fragment() {
     private val downloader = MLKitTranslatorDownloader
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

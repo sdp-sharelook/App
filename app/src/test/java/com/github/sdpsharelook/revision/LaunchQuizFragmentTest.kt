60427e1bf171b95f8e94e6760fa1328a0ec930aa
AdrianUlrich <adrian.schemel@epfl.ch>
2022-05-31 12:10:29 +0200

    fix tests

---
+++ app/src/test/java/com/github/sdpsharelook/revision/LaunchQuizFragmentTest.kt
@@ -0,0 +1,72 @@
+package com.github.sdpsharelook.revision
+
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.sdpsharelook.R
+import com.github.sdpsharelook.utils.FragmentScenarioRule
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.Matchers.*
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class LaunchQuizFragmentTest {
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val fragmentScenarioRule = FragmentScenarioRule.launch(LaunchQuizFragment::class)
+
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun `test buttons`() = runTest {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        fragmentScenarioRule.scenario.onFragment {
+            navController.setGraph(R.navigation.main)
+            navController.setCurrentDestination(R.id.launchQuizFragment)
+            Navigation.setViewNavController(requireView(), navController)
+        }
+        onView(withId(R.id.startQuizButton)).perform(click())
+        advanceUntilIdle()
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(containsString("number"))))
+            .perform(click())
+        onView(withId(R.id.start10QuizButton)).perform(click())
+        assertEquals(R.id.launchQuizFragment, navController.currentDestination!!.id)
+    }
+
+    @Test
+    fun `check navigation`() = runTest {
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        fragmentScenarioRule.scenario.onFragment {
+            navController.setGraph(R.navigation.main)
+            navController.setCurrentDestination(R.id.launchQuizFragment)
+            Navigation.setViewNavController(requireView(), navController)
+        }
+        assertEquals(R.id.launchQuizFragment, navController.currentDestination!!.id)
+        onView(withId(R.id.startAllQuizButton)).perform(click())
+        advanceUntilIdle()
+        assertEquals(R.id.revisionQuizFragment, navController.currentDestination!!.id)
+    }
+}
--- app/src/test/java/com/github/sdpsharelook/revision/RevisionQuizFragmentTest.kt
+++ app/src/test/java/com/github/sdpsharelook/revision/RevisionQuizFragmentTest.kt
@@ -1,20 +1,30 @@
 package com.github.sdpsharelook.revision
 
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.isClickable
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.Visibility.INVISIBLE
+import androidx.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.sdpsharelook.R
 import com.github.sdpsharelook.Word
 import com.github.sdpsharelook.storage.IRepository
 import com.github.sdpsharelook.utils.FragmentScenarioRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.CoreMatchers.anything
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matchers
+import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -40,15 +50,15 @@ class RevisionQuizFragmentTest {
     }
 
     @Test
-    fun `test continue part`() = runTest {
-        advanceUntilIdle()
-        onView(allOf(isClickable(), isDisplayed())).check(ViewAssertions.matches(anything()))
-//        onView(withId(R.id.helpToggleButton)).perform(click())
-//        onView(withId(R.id.answerQualityButton0)).check(matches(withText(any<String>())))
-//        onView(withId(R.id.answerQualityButton1)).check(matches(withText(any<String>())))
-//        onView(withId(R.id.answerQualityButton2)).check(matches(withText(any<String>())))
-//        onView(withId(R.id.answerQualityButton3)).check(matches(withText(any<String>())))
-//        onView(withId(R.id.answerQualityButton4)).check(matches(withText(any<String>())))
-//        onView(withId(R.id.answerQualityButton5)).check(matches(withText(any<String>())))
+    fun `test buttons`() = runTest {
+        onView(withId(R.id.answerQualityButton0)).check(matches(withEffectiveVisibility(INVISIBLE)))
+        onView(withId(R.id.quizLayout)).perform(click())
+        onView(withId(R.id.answerQualityButton0)).check(matches(withEffectiveVisibility(VISIBLE)))
+
+        onView(withId(R.id.answerQualityButton0)).check(matches(withText("")))
+        onView(withId(R.id.helpToggleButton)).perform(click())
+        onView(withId(R.id.answerQualityButton0)).check(matches(withText(not(""))))
+        onView(withId(R.id.helpToggleButton)).perform(click())
+        onView(withId(R.id.answerQualityButton0)).check(matches(withText("")))
     }
 }
